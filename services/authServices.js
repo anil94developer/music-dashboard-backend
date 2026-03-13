1f6a3a7f4b5625b5d2a13b067d4f3e2b44daf8b5
anil94developer <anil94developer@gmail.com>
2026-03-13 17:43:44 +0530

    Change SMTP secure option to false

--- services/authServices.js
+++ services/authServices.js
@@ -18,7 +18,7 @@ const transporter = nodemailer.createTransport({
    
     host: 'smtp.hostinger.com', // Hostinger's SMTP server
   port: 587, // Use 465 for SSL or 587 for STARTTLS
-  secure: true, // Use true for SSL and false for STARTTLS, // Replace with your email service
+  secure: false, // Use true for SSL and false for STARTTLS, // Replace with your email service
     auth: {
         user: process.env.EMAIL_USER, // Your email from environment variables
         pass: process.env.EMAIL_PASSWORD, // Your email password from environment variables
